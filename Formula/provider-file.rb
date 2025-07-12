7d3f5c7d428e5ce95eb22fea52b16c0d26ce5bfb
orpiske <angusyoung@gmail.com>
2025-07-12 08:09:47 +0200

    provider-file v0.0.6

---
+++ Formula/provider-file.rb
@@ -0,0 +1,22 @@
+# Generated with JReleaser 1.17.0 at 2025-07-12T08:05:41.041575+02:00
+
+class ProviderFile < Formula
+  desc "Wanaku MCP Router"
+  homepage "https://wanaku.ai"
+  url "https://github.com/wanaku-ai/wanaku/releases/download/v0.0.6/wanaku-provider-file-0.0.6.zip"
+  version "0.0.6"
+  sha256 "5081458f1b12a488077a261bda17c74cb63786fd91c6970a74df96f2146a1f36"
+  license "Apache-2.0"
+
+  depends_on "openjdk@21"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/provider-file" => "provider-file"
+  end
+
+  test do
+    output = shell_output("#{bin}/provider-file --version")
+    assert_match "0.0.6", output
+  end
+end
