045316e5c7d76e31dbcac1f4babc3dc665f882bb
orpiske <angusyoung@gmail.com>
2025-07-17 11:24:16 +0200

    cli-native v0.0.7

--- Formula/cli-native.rb
+++ Formula/cli-native.rb
@@ -1,13 +1,14 @@
-# Generated with JReleaser 1.17.0 at 2025-07-12T08:30:03.029988+02:00
+# Generated with JReleaser 1.17.0 at 2025-07-17T11:23:41.57021+02:00
 
 class CliNative < Formula
   desc "Wanaku MCP Router"
   homepage "https://wanaku.ai"
-  url "https://github.com/wanaku-ai/wanaku/releases/download/v0.0.6/cli-0.0.6-osx-aarch_64.zip"
-  version "0.0.6"
-  sha256 "6476cfe3380410f99d01fd31fe418c77f0071c2c6ecd32612a630ac45181af3b"
+  url "https://github.com/wanaku-ai/wanaku/releases/download/v0.0.7/cli-0.0.7-osx-aarch_64.zip"
+  version "0.0.7"
+  sha256 "9e2fa32342eb1eb9f29145c81da6399fe8707bfaaa8c4585c114b1f4f7ca8226"
   license "Apache-2.0"
 
+  depends_on "openjdk@21"
 
   def install
     libexec.install Dir["*"]
@@ -16,6 +17,6 @@ class CliNative < Formula
 
   test do
     output = shell_output("#{bin}/cli-native --version")
-    assert_match "0.0.6", output
+    assert_match "0.0.7", output
   end
 end
